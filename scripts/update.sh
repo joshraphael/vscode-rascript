86fc8cde001ed82f0671f1ab54d653d0e8fdde37
Joshua Raphael <josh.n.raphael@gmail.com>
2025-02-22 18:36:46 -0700

    fix update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,12 +6,14 @@ if [[ $(git diff --stat) != '' ]]; then
 else
     if [[ $1 == '--patch' ]]; then
         npm version patch
-        git push -tags origin master
+        git push --tags origin main
     fi
     if [[ $1 == '--minor' ]]; then
         npm version minor
+        git push --tags origin main
     fi
     if [[ $1 == '--major' ]]; then
        npm version major
+       git push --tags origin main
     fi
 fi
